219aa62db46a5444b748658a673e1ad4902d01ff
IvanTHoffmann <55362684+IvanTHoffmann@users.noreply.github.com>
2024-01-27 15:27:33 -0800

    Improved Fluttering Again

--- src/player/fencing_trigger.gd
+++ src/player/fencing_trigger.gd
@@ -1,18 +1,8 @@
 extends Node2D
 
-
 @export var _player = Node2D
 
 signal triggered_fencing()
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 	
 func _on_area_2d_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	emit_signal("triggered_fencing")
--- src/player/player_controller.gd
+++ src/player/player_controller.gd
@@ -4,26 +4,27 @@ extends CharacterBody2D
 @export_category("Character Speed Controls")
 @export var GROUND_SPEED = 400
 @export var STOP_SPEED = 300
-@export var AIR_ACCEL = 200
+@export var AIR_ACCEL = 1000
 @export var AIR_DECEL = 2000
 @export var FLAP_FORCE = 2500
 @export var FLAP_GRAVITY_MOD = .7
 
 @export var FENCING_LUNGE_COOLDOWN = .5
 @export var FENCING_LUNGE_SPEED = 650
+@export var FENCING_LUNGE_ARM_OFFSET = 15
 @export var FENCING_HOP_SPEED = 125
 
 @export var MIN_GUST_SPEED = 8
-@export var MAX_GUST_SPEED = 20
+@export var MAX_GUST_SPEED = 30
 
 @export var JUMP_VELOCITY = -800
-@export var MAX_SPEED = 1000
+@export var MAX_SPEED = 600
 
-@export var FLAP_COOLDOWN = 2
+@export var FLAP_COOLDOWN = 2.0
 @export var ARM_SPEED_BUFFER = 10
 
 @export var GRAVITY_MUL = 1.9
-@export var GRAVITY_RAMP = 300
+@export var GRAVITY_RAMP = 500
 @export var GRAVITY_MIN = 1.0
 
 @export var FLY_ANIMATION = "TestFlyAnimation"
@@ -31,8 +32,6 @@ extends CharacterBody2D
 @export var FLAP_ANIMATION = "TestIdleAnimation"
 @export var FENCING_ANIMATION = "TestFencingAnimation"
 @export var FALL_ANIMATION = "TestFencingAnimation"
-
-@export var _gust_pool : Node
 
 @onready var _animated_sprite = $AnimatedSprite2D
 @onready var _arm = $Arm
@@ -57,61 +56,65 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _get_gravity(y_vel):
 	return GRAVITY_MUL * gravity * (GRAVITY_MIN + 1 - clamp(GRAVITY_RAMP - abs(y_vel), 0, GRAVITY_RAMP) / GRAVITY_RAMP)
 	
-func _process(delta):
+func _process(_delta):
 	# update body
 	_animated_sprite.flip_h = (get_global_mouse_position().x - position.x < 0)
 	
-func _rotate_feather(delta):
+func _rotate_feather(delta, timer):
 	# rotate the feather about the shoulder
 	var angle_diff = _arm.get_angle_to(get_global_mouse_position())
-	_arm.rotation += angle_diff
+	_arm.rotation += angle_diff + int(Input.is_action_pressed("Flutter")) * .6 * sin(timer * 50)
 	_ave_arm_speed = (_ave_arm_speed * ARM_SPEED_BUFFER + abs(angle_diff) / delta) / (ARM_SPEED_BUFFER + 1)
 	
-func _slide_feather(delta):
+func _slide_feather(_delta, timer):
 	# slide the feather up and down for fencing
 	_arm.rotation = int(get_global_mouse_position().x < position.x) * PI
-	_arm.position.y = clamp((get_global_mouse_position().y - position.y), -30, 30)
+	_arm.position.y = clamp((get_global_mouse_position().y - position.y), -30, 30) 
+	_arm.position.y += 2 * sin(timer*6)
+	
+func _clamp_speed():
+	if abs(velocity.x) > MAX_SPEED:
+		velocity.x = sign(velocity.x) * MAX_SPEED
 
 func _set_state_grounded():
-	print("grounded")
-	_animated_sprite.stop()
+	_set_state()
 	_animated_sprite.play(IDLE_ANIMATION)
 	_state = STATES.GROUNDED
-	_state_timer = 0
 
 func _set_state_jumping():
-	print("jumping")
-	_animated_sprite.stop()
+	_set_state()
 	_animated_sprite.play(FLY_ANIMATION)
 	_state = STATES.JUMPING
-	_state_timer = 0
 	
 func _set_state_falling():
-	print("falling")
-	_animated_sprite.stop()
+	_set_state()
 	_animated_sprite.play(FALL_ANIMATION)
 	_state = STATES.FALLING
-	_state_timer = 0
 	
 func _set_state_flapping():
 	if _state == STATES.FLAPPING:
 		return
-	print("flapping")
-	_animated_sprite.stop()
+	_set_state()
 	_animated_sprite.play(FLAP_ANIMATION)
 	_state = STATES.FLAPPING
-	_state_timer = 0
 	
 func _set_state_jousting():
-	print("jousting")
-	_animated_sprite.stop()
+	_set_state()
 	_animated_sprite.play(FENCING_ANIMATION)
 	_state = STATES.FENCING
+	
+func _set_state():
 	_state_timer = 0
+	_arm.position.x = 0
+	_arm.position.y = 0
+	_animated_sprite.stop()
+	
 	
 func _process_arm(delta):
 	if MIN_GUST_SPEED < _ave_arm_speed and _ave_arm_speed < MAX_GUST_SPEED:
 		var arm_direction = Vector2.from_angle(_arm.rotation)
+		if velocity.y > 0:
+			velocity.y = 0
 		velocity -= arm_direction * FLAP_FORCE * delta
 		_set_state_flapping()
 
@@ -132,11 +135,11 @@ func _process_state_grounded(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, STOP_SPEED)
 		
-	_rotate_feather(delta)
+	_rotate_feather(delta, _state_timer)
 	_process_arm(delta)
 	
 func _process_state_jumping(delta):
-	_rotate_feather(delta)
+	_rotate_feather(delta, _state_timer)
 	_process_arm(delta)
 	if is_on_floor():
 		_set_state_grounded()
@@ -153,10 +156,7 @@ func _process_state_jumping(delta):
 		else:
 			velocity.x += xinput * AIR_ACCEL * delta
 		
-	if abs(velocity.x) > MAX_SPEED:
-		velocity.x = sign(velocity.x) * MAX_SPEED
-	if velocity.y < -MAX_SPEED:
-		velocity.y = -MAX_SPEED
+	_clamp_speed()
 	
 func _process_state_flapping(delta):
 	if is_on_floor():
@@ -165,25 +165,38 @@ func _process_state_flapping(delta):
 	if _state_timer > FLAP_COOLDOWN:
 		_set_state_falling()
 		return
+		
+	# Get the input direction and handle the movement/deceleration.
+	var xinput = Input.get_axis("left", "right")
+	if xinput:
+		if sign(velocity.x) != sign(xinput):
+			velocity.x += xinput * AIR_DECEL * delta
+		else:
+			velocity.x += xinput * AIR_ACCEL * delta
+		
 	velocity.y += _get_gravity(velocity.y) * delta * FLAP_GRAVITY_MOD
-	_rotate_feather(delta)
+	_rotate_feather(delta, _state_timer)
 	_process_arm(delta)
+	
+	_clamp_speed()
+	if velocity.y < -MAX_SPEED:
+		velocity.y = -MAX_SPEED
 	
 func _process_state_falling(delta):
 	if is_on_floor():
 		_set_state_grounded()
 		return
 	velocity.y += _get_gravity(velocity.y) * delta
-	_rotate_feather(delta)
+	_rotate_feather(delta, _state_timer)
 	
 func _process_state_fencing(delta):
-	
-	_slide_feather(delta)
+	_slide_feather(delta, _state_timer)
 	
 	velocity.y += _get_gravity(velocity.y) * delta
 	
 	var xinput = Input.get_axis("left", "right")
 	if is_on_floor():
+		_arm.position.x = 0
 		if xinput and _state_timer > FENCING_LUNGE_COOLDOWN * .5:
 			velocity.x = xinput * GROUND_SPEED
 			velocity.y -= FENCING_HOP_SPEED
@@ -191,8 +204,10 @@ func _process_state_fencing(delta):
 	
 		elif Input.is_action_pressed("jump") and _state_timer > FENCING_LUNGE_COOLDOWN:
 			# lunge
-			velocity.x = sign(get_global_mouse_position().x - position.x) * FENCING_LUNGE_SPEED
+			var facing_dir = sign(get_global_mouse_position().x - position.x)
+			velocity.x = facing_dir * FENCING_LUNGE_SPEED
 			velocity.y -= FENCING_HOP_SPEED
+			_arm.position.x = FENCING_LUNGE_ARM_OFFSET * facing_dir
 			_state_timer = 0
 			
 		else:
