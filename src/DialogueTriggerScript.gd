dd86b320e3adfdbed585f336d829ab55945c44b0
Smoresies <gavin.britton@gmail.com>
2024-01-27 17:28:55 -0800

    Fixed Dialogue Trigger

--- src/DialogueTriggerScript.gd
+++ src/DialogueTriggerScript.gd
@@ -10,6 +10,7 @@ func _ready():
 func _on_body_entered(body):
 	#I have no clue how to check for if the body is the player, but that check would go here.
 	#No clue how to disable the player's controls/pause the game during dialogue, but that should happen. Prolly handle via signal in Player Controller?
+	print(body.name)
 	Dialogic.start(TIMELINE_NAME)
 	$CollisionShape2D.set_deferred("disabled", true)
 
