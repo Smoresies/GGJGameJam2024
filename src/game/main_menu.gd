ae74d2b6502b7ce8aec7987b1c8cedda33602b47
Ethan Kelly <ekellysnoco@gmail.com>
2024-01-28 12:07:43 -0800

    Implemented all menu sounds!

--- src/game/main_menu.gd
+++ src/game/main_menu.gd
@@ -1,18 +1,66 @@
 extends Control
 
+func _ready():
+	## Play the opening audio cue:
+	$Audio/sfx_ui_mainmenu_opening.play()
+	## Reveal the team logo
+	## Journal comes into frame, opens and we see the menu
 
-# Called when the node enters the scene tree for the first time.
+func _on_sfx_ui_mainmenu_opening_finished():
+	## Now we play the main menu theme
+	$Audio/music_mainmenu.play()
+
+## ---
+
 func _on_button_pressed():
+	$Audio/sfx_ui_mainmenu_play.play() 
+	## Wait for sound to finish...
+
+func _on_sfx_ui_mainmenu_play_finished():
 	SceneManager.SwitchScene("TutorialLevel")
 
+## ---
 
 func _on_quit_pressed():
+	$Audio/sfx_ui_mainmenu_quit.play()
+	##  Wait for the journal to shut before quitting. See func below
+	## get_tree().quit()
+
+func _on_sfx_ui_mainmenu_quit_finished():
 	get_tree().quit()
 
-
 func _on_main_menu_pressed():
+	sfx_play_click_sound()
+	## Wait for the  journal pages to turn:
 	SceneManager.SwitchScene("MainMenu")
 
+## ---
 
 func _on_credits_pressed():
+	$Audio/sfx_ui_mainmenu_credits.play()
+	## Wat for the sound to finish before changing scenes: 
+	
+func _on_sfx_ui_mainmenu_credits_finished():
 	SceneManager.SwitchScene("Credits")
+
+## ---
+
+func _on_button_mouse_entered():
+	sfx_play_hover_sound()
+
+func _on_credits_mouse_entered():
+	sfx_play_hover_sound()
+
+func _on_quit_mouse_entered():
+	sfx_play_hover_sound()
+
+## Custom functions for audio:
+func sfx_play_click_sound():
+	$Audio/sfx_ui_mainmenu_buttonclick.play()
+
+func sfx_play_hover_sound():
+	$Audio/sfx_ui_mainmenu_buttonhover.play()
+
+
+
+
--- src/game/pause_menu.gd
+++ src/game/pause_menu.gd
@@ -17,15 +17,49 @@ func _on_game_manager_toggle_game_paused(is_paused: bool):
 	else:
 		hide()
 
+## ---
 
 func _on_resume_button_pressed():
+	$Audio/sfx_ui_pausemenu_resume.play()
+	## Wait for the sound to finish playing before unloading the scene: 
+
+func _on_sfx_ui_pausemenu_resume_finished():
 	game_manager.game_paused = false
 
+### ---
 
 func _on_quit_button_pressed():
+	$Audio/sfx_ui_pausemenu_quit.play()
+	## Wait for the sound to finish playing before unloading the scene: 
+
+func _on_sfx_ui_pausemenu_quit_finished():
 	get_tree().quit()
 
+## ---
 
 func _on_main_menu_button_pressed():
+	$Audio/sfx_mainmenu_buttonclick.play()
+	## Wait for the sound to finish playing before unloading this scene
+
+func _on_sfx_mainmenu_buttonclick_finished():
 	game_manager.game_paused = false
 	SceneManager.SwitchScene("MainMenu")
+
+## ---
+
+func sfx_play_hover_sound():
+	$Audio/sfx_mainmenu_buttonhover.play()
+
+func _on_resume_button_mouse_entered():
+	sfx_play_hover_sound()
+
+func _on_main_menu_button_mouse_entered():
+	sfx_play_hover_sound()
+
+func _on_quit_button_mouse_entered():
+	sfx_play_hover_sound()
+
+
+
+
+
