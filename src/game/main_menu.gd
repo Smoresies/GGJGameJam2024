820d5c3535ef14d47467c5d157eb295bb38b8d2d
Smoresies <gavin.britton@gmail.com>
2024-01-28 10:38:16 -0800

    Changed Main Menu, added credits.

--- src/game/main_menu.gd
+++ src/game/main_menu.gd
@@ -4,3 +4,15 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _on_button_pressed():
 	SceneManager.SwitchScene("TutorialLevel")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+
+func _on_main_menu_pressed():
+	SceneManager.SwitchScene("MainMenu")
+
+
+func _on_credits_pressed():
+	SceneManager.SwitchScene("Credits")
